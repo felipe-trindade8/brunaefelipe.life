6c900d49c7eed49f34cff789243f2f1a32261e67
Felipe Trindade <felipe.trindade8@outlook.com>
2019-02-28 00:35:28 +0000

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Bruna e Felipe - Save the date!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="theme/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="theme/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="theme/css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="theme/css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="theme/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="theme/css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="theme/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="theme/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="theme/js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row top">
+				<div class="col-xs-3">
+					<div id="fh5co-logo"><a href="index.html">Bru <i class="icon-heart2" style="font-size: 26px;"></i> Fe</a></div>
+				</div>
+				<div class="col-xs-9 text-right menu-1">
+					<ul>
+						<li class="active"><a href="#top">Início</a></li>
+						<li><a href="/lista-presenca">Confirmação de presença</a></li>
+						<li><a href="/lista-loja">Lista de presentes</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(theme/images/bg.jpg); background-position:center;">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Bruna &amp; Felipe</h1>
+							<h2>Vamos nos casar!</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<p><a href="/lista-presenca" class="btn btn-default btn-sm">Confirme sua presença!</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-couple" class="fh5co-section-gray">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Save the date!</h2>
+					<h3>24 de Junho de 2019</h3>
+				</div>
+			</div>
+			<div class="couple-wrap animate-box">
+				<div class="couple-half">
+					<div class="groom">
+						<img src="theme/images/bruna.jpg" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-groom">
+						<h3>Bruna</h3>
+						<p>
+							Publicitária, noveleira e adooooooora tirar fotos do céu! Curte filmes românticos e sempre chora no final. Tem um repertório gigantesco de frases "fofinhas". Seu ponto fraco: matemática.
+						</p>
+					</div>
+				</div>
+				<p class="heart text-center"><i class="icon-heart2"></i></p>
+				<div class="couple-half">
+					<div class="bride">
+						<img src="theme/images/felipe.jpg" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-bride">
+						<h3>Felipe</h3>
+						<p>
+							Programador, fanático pelo Corinthians e por futebol. Cheddar e bacon não podem faltar no seu cardápio. É apaixonado por motos, porém NÍNGUEM deixa ele ter uma novamente (mas também neh?!). Adora séries (Séria A do Brasileirão ¬¬).
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-couple-story">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Nossa história</h2>
+					<p>"O que importa no final das contas, quantas rosas ele te deu ou quantos sorrisos te causou? Você prefere o carinho dele ou apenas um romance de filme? Olhe para quem está do teu lado e se atenha aos detalhes, veja como o amor está em cada gesto, cada olhar e cada sorriso. O amor está ai, em sua forma mais verdadeira, mais simples e apaixonante."</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0">
+					<ul class="timeline animate-box">
+						<li class="animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-1.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Nascimento da Bruna</h3>
+									<span class="date">13 de Maio de 1993</span>
+								</div>
+								<div class="timeline-body">
+									<p>Apressada, resolveu nascer um pouquinho antes da hora, dando trabalho para toda a família desde o dia que nasceu. Aparentemente, esquecia que tinha que respirar.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-2.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Nascimento do Felipe</h3>
+									<span class="date">15 de Julho de 1994</span>
+								</div>
+								<div class="timeline-body">
+									<p>O noivo teve que esperar mais uns dias para nascer, já que seu pai tinha um jogo de futebol importante no dia marcado pelo médido.</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-3.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Moleca na Infância</h3>
+									<span class="date">Dos 3 aos 14 anos</span>
+								</div>
+								<div class="timeline-body">
+									<p>Ao contrário de suas primas, preferia jogar bola na rua à brincar de bonecas. Joelho ralado era uma marca constante nessa fase. Mamãe adorava quando chegava com os pés pretos em casa #sqn.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-3.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title"></h3>
+									<span class="date">Dos 3 aos 14 anos</span>
+								</div>
+								<div class="timeline-body">
+									<p>Futebol e video game eram seu passatempo. Quando não, estava aprontando alguma. As principais: fazer xixi na fruteira e tomar banho de óleo antes da festa de aniversário. Seu lugar favorito? Atrás do sofá ( ͡° ͜ʖ ͡°).</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-3.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">A primeira impressão</h3>
+									<span class="date">Agosto de 2010</span>
+								</div>
+								<div class="timeline-body">
+									<p>Nos conhecemos no Philadelpho onde fizemos o curso técnico juntos. A primeira impressão foi de um menino metido e isolado, que por já ter feito outro curso, se achava o mais inteligente da turma.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-3.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">A amizade que virou casamento</h3>
+									<span class="date">24 de Junho de 2011</span>
+								</div>
+								<div class="timeline-body">
+									<p>Não foi como ela imaginou, um jantar romântico ou ao menos um cinema, mas sim na Praça Rui Barbosa, no centro da cidade onde nos beijamos pela primeira vez.</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box">
+							<div class="timeline-badge" style="background-image:url(theme/images/couple-3.jpg);"></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">O pedido</h3>
+									<span class="date">24 de Dezembro de 2018</span>
+								</div>
+								<div class="timeline-body">
+									<p>Para compensar, o pedido de noivado foi em grande estilo: na areia da praia, nosso lugar favorito.</p>
+								</div>
+							</div>
+						</li>
+			    	</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div id="fh5co-event" class="fh5co-bg" style="background-image:url(theme/images/img_bg_3.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<span>O grande dia</span>
+					<h2>Nosso Casamento</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="col-md-10 col-md-offset-1">
+							<div class="col-md-8 col-sm-8 text-center" style="float: none; margin: 0 auto;">
+								<div class="event-wrap animate-box">
+									<h3>Cerimônia + Festa</h3>
+									<div class="event-col">
+										<i class="icon-clock"></i>
+										<span>19:30</span>
+										<span>Dai - Cucina & Bar</span>
+									</div>
+									<div class="event-col">
+										<i class="icon-calendar"></i>
+										<span>Segunda 24</span>
+										<span>Junho, 2019</span>
+									</div>
+									<p>Em 24 de Junho de 2011, nossa história começou, e exatamente 8 anos depois ela ganhará um novo capítulo. Então reserve essa data em seu calendário para que possamos juntos celebrar a nossa união.</p>
+									<p><a target="_blank" href="https://www.google.com/maps/dir//Dai+-+Cucina+%26+Bar+-+Av+alberto+andal%C3%B3,+3209+-+Centro,+S%C3%A3o+Jos%C3%A9+do+Rio+Preto+-+SP,+15015-000/@-20.817675,-49.3859475,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x94bdad5922d0c2c1:0x5d734b21cc4d777e!2m2!1d-49.379412!2d-20.815426!3e0" class="btn btn-default btn-sm">Como chegar</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<h2>Mural de Recados</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<div class="wrap-testimony">
+							<div class="owl-carousel-fullwidth">
+								@foreach ($recados as $recado)
+									<div class="item">
+										<div class="testimony-slide active text-center">
+											<figure>
+												<img src="{{ secure_asset('storage/' . $recado->foto) }}" alt="{{ $recado->autor }}">
+											</figure>
+											<span>{{ $recado->autor }}, {{ $recado->relacao }}</span>
+											<blockquote>
+												<p>"{{ $recado->recado }}"</p>
+											</blockquote>
+										</div>
+									</div>
+								@endforeach
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<blockquote>
+											<p>Tem alguma história com a gente? Algum fato engraçado? Conte-nos ou deixe um recadinho para nós!</p>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+							
+							<p><a href="" class="btn btn-default btn-sm" style="display: block;width: 250px;margin: 0 auto;background: #F14E95;color: #FFFFFF;">Enviar um Recadinho</a></p>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo" style="padding: 0;">
+		<div class="container">
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">Feito com <i class="icon-heart2" style="color: #f14e95;"></i> por Bruna & Felipe</small>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="theme/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="theme/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="theme/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="theme/js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="theme/js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="theme/js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="theme/js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="theme/js/jquery.magnific-popup.min.js"></script>
+	<script src="theme/js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="theme/js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="theme/js/main.js"></script>
+
+	<script>
+    var d = new Date('2019-06-24 19:30:00');
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        words: {
+	        days: 'dia',
+	        hours: 'hora',
+	        minutes: 'minuto',
+	        seconds: 'segundo',
+	        pluralLetter: 's'
+        },
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+</script>
+
+	</body>
+</html>
+
