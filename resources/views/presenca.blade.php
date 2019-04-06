ab2827f6ed27046801d667b7a3be0e66cef32810
Felipe Trindade <felipe.trindade8@outlook.com>
2019-04-05 22:31:43 -0300

    Atualizando projeto.

--- resources/views/presenca.blade.php
+++ resources/views/presenca.blade.php
@@ -61,13 +61,13 @@
         </div>
     </nav>
 
-    <header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(theme/images/bg.jpg); background-position:center; height: 400px;">
+    <header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(theme/images/bg.jpg); background-position:center;">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 text-center">
-                    <div class="display-t" style="height: 400px;">
-                        <div class="display-tc animate-box" data-animate-effect="fadeIn" style="height: 400px;">
+                    <div class="display-t">
+                        <div class="display-tc animate-box" data-animate-effect="fadeIn">
                             <h1>Confirme sua presença</h1>
                             <h2>Ela é muito importante para nós.</h2>
                             <div class="simply-countdown simply-countdown-one"></div>
